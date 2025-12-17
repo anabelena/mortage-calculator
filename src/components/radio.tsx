fd285c1bb7bf9fb73dcd97615df8016a69c294b2
anabelena <anabelen.aristah@gmail.com>
2025-12-17 13:44:25 -0800

    feat(radio): style custom radio using peer and checked state (tailwind)

--- src/components/radio.tsx
+++ src/components/radio.tsx
@@ -8,10 +8,16 @@ export const Radio = ({ label }: Props) => {
   return (
     <label
       htmlFor={id}
-      className="font-bold text-xl text-Slate-900 border border-Slate-500 rounded-sm h-[46px]  flex items-center gap-2 px-2 mb-3.5 cursor-pointer hover:border-Lime hover:bg-Lime/10 has-checked:bg-Lime/10 has-checked:border-Lime"
+      className="font-bold text-xl text-Slate-900 border border-Slate-500 rounded-sm h-[46px]  flex items-center gap-2 px-2 mb-3 cursor-pointer hover:border-Lime hover:bg-Lime/10 has-checked:bg-Lime/10 has-checked:border-Lime"
     >
-      <div className="">
-        <input name="mortgageType" type="radio" id={id} className="appearance-none size-5 border checked:border-Lime rounded-full" />
+      <div className="relative ">
+        <input
+          name="mortgageType"
+          type="radio"
+          id={id}
+          className="peer appearance-none size-5 border checked:border-Lime rounded-full "
+        />
+        <span className="size-3 absolute bg-Lime rounded-full top-1.5 left-1 opacity-0 peer-checked:opacity-100"></span>
       </div>
       <span>{label}</span>
     </label>
