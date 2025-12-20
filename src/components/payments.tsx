d40ff47b990374d6c6cb56142da817951ec1dbae
anabelena <anabelen.aristah@gmail.com>
2025-12-19 18:23:50 -0800

    feat(results): component updated, add call from results component.

---
+++ src/components/payments.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  styles?: string;
+}
+
+export const Payments = ({ styles = "" }: Props) => {
+  return (
+    <div className={`${styles}`}>
+      <div className="bg-Slate-900 border-t-4 border-Lime rounded-lg px-5 py-6">
+        <h3 className="mb-3"> Your monthly repayments </h3>
+        <p className="text-Lime font-bold text-4xl">
+          <span> CAD </span> 1,250.00{" "}
+        </p>
+        <hr />
+        <h3 className="mb-3"> Total you'll repay over the term</h3>
+        <p className="font-bold text-2xl">
+          <span> CAD </span> 539,322,94
+        </p>
+      </div>
+    </div>
+  );
+};
