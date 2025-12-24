78c0209bc85cda1901b3e64de6b5bbac4ace19fb
anabelena <anabelen.aristah@gmail.com>
2025-12-23 17:57:10 -0800

    feat(payments,results): add component payment, add call to component from results

--- src/components/payments.tsx
+++ src/components/payments.tsx
@@ -5,14 +5,14 @@ interface Props {
 export const Payments = ({ styles = "" }: Props) => {
   return (
     <div className={`${styles}`}>
-      <div className="bg-Slate-900 border-t-4 border-Lime rounded-lg px-5 py-6">
-        <h3 className="mb-3"> Your monthly repayments </h3>
-        <p className="text-Lime font-bold text-4xl">
+      <div className="bg-Slate-900 text-Slate-300 border-t-4 border-Lime rounded-lg px-5 py-6">
+        <h3  className="mb-3"> Your monthly repayments </h3>
+        <p className="text-Lime font-bold text-4xl mb-4">
           <span> CAD </span> 1,250.00{" "}
         </p>
-        <hr />
+        <hr className="mb-4" />
         <h3 className="mb-3"> Total you'll repay over the term</h3>
-        <p className="font-bold text-2xl">
+        <p className="font-bold text-2xl text-white">
           <span> CAD </span> 539,322,94
         </p>
       </div>
--- src/components/results.tsx
+++ src/components/results.tsx
@@ -11,7 +11,7 @@ export const Results = ({ styles = "" }: Props) => {
         className={`${styles}bg-Slate-900/90 text-white px-6 py-8`}
       >
         <h2 className=" text-2xl font-bold mb-4 "> Your results</h2>
-        <p className="text-slate-300 mb-4">
+        <p className="text-slate-300 mb-5">
           Your results are shown below based on the information you provided. To
           adjust the results, edit the form and click "calculate repayments"
           again.
