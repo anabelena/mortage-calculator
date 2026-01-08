e3c6356e7d51c372e6d2111206eef0e2f5086ec3
anabelena <anabelen.aristah@gmail.com>
2026-01-08 14:09:52 -0800

    feat(ui): responsive design for sm, md and lg screens done.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { Calculator } from "./components"
 function App() {
 
   return (
-    <>
+    <main className="lg:h-screen lg:grid lg:place-content-center">
      <Calculator/>
-    </>
+    </main>
   )
 }
 
--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Calculator = ({ styles = "" }: Props) => {
   return (
     <div
-      className={` ${styles} md:max-w-[688px] md:my-10 md:rounded-2xl md:border-Slate-900 md:border overflow-hidden lg:flex lg:max-w-[1008px]`}
+      className={` ${styles} md:max-w-[688px] md:my-10 md:rounded-2xl   overflow-hidden lg:flex lg:max-w-[1008px]`}
     >
       {/* Mortage Calculator Form */}
       <Form styles="lg:flex-1" />
