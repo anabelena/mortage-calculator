adfefe822559164e7238bfd6371782275665e8c3
anabelena <anabelen.aristah@gmail.com>
2026-01-21 17:36:27 -0800

    refactor(radio): updated props and ref propertie deleted

--- src/components/radio.tsx
+++ src/components/radio.tsx
@@ -1,32 +1,28 @@
-import { forwardRef } from "react";
-
-interface Props {
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   value: string;
 }
 
-export const Radio = forwardRef<HTMLInputElement, Props>(
-  ({ label, value, ...props }, ref) => {
-    const id = label.replace(/\s+/g, "-").toLowerCase();
+export const Radio = ({ label, value, ...props }: Props) => {
+  //Generate id
+  const id = label.replace(/\s+/g, "-").toLowerCase();
 
-    return (
-      <label
-        htmlFor={id}
-        className="font-bold text-xl text-Slate-900 border border-Slate-500 rounded-sm h-[46px]  flex items-center gap-2 px-2 mb-3 cursor-pointer hover:border-Lime hover:bg-Lime/10 has-checked:bg-Lime/10 has-checked:border-Lime"
-      >
-        <div className="relative ">
-          <input
-            ref={ref}
-            id={id}
-            type="radio"
-            value={value}
-            {...props}
-            className="peer appearance-none size-5 border checked:border-Lime rounded-full "
-          />
-          <span className="size-3 absolute bg-Lime rounded-full top-1.5 left-1 opacity-0 peer-checked:opacity-100"></span>
-        </div>
-        <span>{label}</span>
-      </label>
-    );
-  },
-);
+  return (
+    <label
+      htmlFor={id}
+      className="font-bold text-xl text-Slate-900 border border-Slate-500 rounded-sm h-[46px]  flex items-center gap-2 px-2 mb-3 cursor-pointer hover:border-Lime hover:bg-Lime/10 has-checked:bg-Lime/10 has-checked:border-Lime"
+    >
+      <div className="relative ">
+        <input
+          id={id}
+          type="radio"
+          value={value}
+          {...props}
+          className="peer appearance-none size-5 border checked:border-Lime rounded-full "
+        />
+        <span className="size-3 absolute bg-Lime rounded-full top-1.5 left-1 opacity-0 peer-checked:opacity-100"></span>
+      </div>
+      <span>{label}</span>
+    </label>
+  );
+};
